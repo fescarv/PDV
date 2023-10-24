996391c88b9b24c5cbb96eeb5d8d6117d02422c0
fescarvalho <fernando_scarvalho@hotmail.com>
2023-10-24 10:38:24 -0300

    Finalizados todos os endpoints da sprint 2

---
+++ src/controllers/produtos/cadastrarProduto.js
@@ -0,0 +1,26 @@
+const knex = require('../../../conexao');
+
+const cadastrarProduto = async (req, res) => {
+  const { categoria_id } = req.body;
+  try {
+    const categoriaExists = await knex('categorias')
+      .where('id', categoria_id)
+      .first();
+
+    if (!categoriaExists)
+      return res.status(400).json({ mensagem: 'Categoria não existe!' });
+
+    const newProduct = {
+      ...req.body,
+    };
+
+    await knex('produtos').insert(newProduct);
+    return res
+      .status(201)
+      .json({ mensagem: 'Produto cadastrado com sucesso!' });
+  } catch (error) {
+    return res.status(200).json({ mensagem: error.message });
+  }
+};
+
+module.exports = cadastrarProduto;
--- src/utils/validations/schemaUsario.js
+++ src/utils/validations/schemaUsario.js
@@ -11,4 +11,31 @@ const schemaUsario = yup.object({
     .required('O campo senha é obrigatório.'),
 });
 
-module.exports = schemaUsario;
+const schemaProduto = yup.object({
+  descricao: yup.string().required('O campo descrição é obrigatório.'),
+  quantidade_estoque: yup
+    .number()
+    .required('O campo quantidade é obrigatório.'),
+  valor: yup.number().required('O campo valor é obrigatório.'),
+  categoria_id: yup.number().required('O campo categoria_id é obrigatório.'),
+});
+
+const schemaCliente = yup.object({
+  nome: yup.string().required('O campo nome é obrigatório.'),
+  email: yup
+    .string()
+    .email('Por favor, insira um email válido.')
+    .required('O campo email é obrigatório.'),
+  cpf: yup
+    .string()
+    .required('O campo CPF é obrigatório.')
+    .min(9, 'CPF inválido.')
+    .max(9, 'CPF inválido.'),
+  cep: yup.string().min(8, 'CEP inválido.').max(8, 'CEP inválido.'),
+  rua: yup.string(),
+  numero: yup.number(),
+  bairro: yup.string(),
+  cidade: yup.string(),
+  estado: yup.string(),
+});
+module.exports = { schemaUsario, schemaProduto, schemaCliente };
